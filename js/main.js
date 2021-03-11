ab397ac5e6cf367c79605a37ac87835ffeaa61b7
Renato Picco <56326785+repicco@users.noreply.github.com>
2021-03-11 00:51:08 -0300

    Refatoração

--- js/main.js
+++ js/main.js
@@ -4,32 +4,27 @@ function calcAlimento ( ) {
     let vegetariano = document.getElementById('veg').value
     let horas = document.getElementById('hora').value
 
-    let Carne = ((carnivoro * 0.4)) * (horas / 4)
-    
-    let Vegetal = ((vegetariano * 0.1) + (carnivoro * 0.15)) * (horas / 4)
-
-    let Cerveja = ((carnivoro + vegetariano) * 0.5) * (horas / 4)
-
-    let Refri = ((carnivoro + vegetariano) * 0.5) * (horas / 4)
-
-    Total = [Carne, Vegetal, Cerveja, Refri]
-
-    console.log (Total)
-
-    let totalCarne = Total[0]
-    let totalVegetal = Total[1]
-    let totalCerveja = Total[2]
-    let totalRefri = Total[3]
-
-    totalCarne = parseFloat(totalCarne.toFixed(2))
-    totalVegetal = parseFloat(totalVegetal.toFixed(2))
-    totalCerveja = parseFloat(totalCerveja.toFixed(3))
-    totalRefri = parseFloat(totalRefri.toFixed(3))
+    function calc(value){
+        let total = 0
+        if(value == 'Carne'){
+            total = (carnivoro * 0.4) * (horas / 4)
+        } else if(value == 'Vegetal'){
+            total = (vegetariano * 0.4 + carnivoro * 0.15) * (horas / 4)
+        } else {
+            total = (carnivoro + vegetariano) * 0.5 * (horas / 4)
+            return parseFloat(total.toFixed(3))
+        }
+        return parseFloat(total.toFixed(2))
+    }
+      
+    let totalCarne = calc('Carne')
+    let totalVegetal = calc('Vegetal')
+    let totalBebida = calc()
 
     document.getElementById('carne').innerHTML = totalCarne + ' kg'
     document.getElementById('salada').innerHTML = totalVegetal + ' kg'
-    document.getElementById('cerveja').innerHTML = totalCerveja + ' l'
-    document.getElementById('refrigerante').innerHTML = totalRefri + ' l'
+    document.getElementById('cerveja').innerHTML = totalBebida + ' l'
+    document.getElementById('refrigerante').innerHTML = totalBebida + ' l'
 }
 
 
